68f95c56fffc8c80995e2c3cab751d6f2cbf504c
Srimathi H <srimathih@sahajsoft.com>
2018-04-21 13:19:56 +0530

    Changing port to 7545

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -56,7 +56,7 @@ window.addEventListener('load', function() {
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:7545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
   }
 
   App.start();
